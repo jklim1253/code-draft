6be0d856308c58a32f8be2971e8aed506ecb8720
Jong Kyu, Lim <jklim1253@gmail.com>
2016-01-10 16:39:55 +0900

    thread code added. - wrapper of std::thread. - default thread running type : detach - force_quit : adjust thread running type. - get_id : thread::get_id

---
+++ thread/thread/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <thread>
+#include <string>
+using namespace std;
+
+void function_1() {
+	cout << "Beauty is only skin-deep" << endl;
+}
+
+class Functor {
+public :
+	void operator()() {
+		for (int i = 0; i > -50; --i) {
+			cout << "from t1 : " << i << endl;
+		}
+	}
+};
+
+class Thread {
+private :
+	std::thread t1;
+	bool _force_quit;
+public :
+	template<typename Fn, typename... Args>
+	Thread(Fn&& fn, Args&&... args) : t1(fn, args...), _force_quit(true) {
+
+	}
+	Thread() : _force_quit(true) {
+
+	}
+	void force_quit(bool quit) {
+		_force_quit = quit;
+	}
+	std::thread::id get_id() const noexcept {
+		return t1.get_id();
+	}
+	Thread(const Thread&) = delete;
+	Thread& operator=(const Thread&) = delete;
+	~Thread() {
+		if (t1.joinable()) {
+			if (_force_quit)
+				t1.detach();
+			else
+				t1.join();
+		}
+	}
+};
+
+int main() {
+	Thread t1((Functor()));
+
+	//t1.force_quit(false);
+
+	for (int i = 0; i < 100; ++i) {
+		cout << "from main : " << i << endl;
+	}
+
+	cout << t1.get_id() << endl;
+
+	return 0;
+}
