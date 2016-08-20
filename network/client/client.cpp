4421ee748f61e6531fe010e6b9a405031ef41be5
Jong Kyu, Lim <jklim1253@gmail.com>
2016-08-21 05:16:53 +0900

    server/client 구현부 추가.

--- network/client/client.cpp
+++ network/client/client.cpp
@@ -1,5 +1,6 @@
 #define _WIN32_WINNT 0x0601
 #include <string>
+#include <thread>
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
@@ -17,15 +18,49 @@ private :
 	boost::asio::io_service& ios_;
 };
 
+#pragma pack(1)
+
+struct Header {
+	unsigned long marker;
+	unsigned long data_type;
+	unsigned long data_size;
+};
+struct Data {
+	unsigned long marker;
+	unsigned long data_type;
+	std::shared_ptr<unsigned char> buffer;
+};
+struct Packet {
+	Header header;
+	Data data;
+	void encode() {
+		header.marker = 0x01020304;
+		data.marker = 0x04030201;
+	}
+	bool header_valid() const {
+		unsigned long validation = 0x01020304;
+		if (::memcmp(&header.marker, &validation, sizeof(unsigned long)) == 0) {
+			return true;
+		}
+		return false;
+	}
+	bool data_valid() const {
+		unsigned long validation = 0x04030201;
+		if (::memcmp(&data.marker, &validation, sizeof(unsigned long)) == 0) {
+			return true;
+		}
+		return false;
+	}
+};
 struct Session {
 	std::shared_ptr<boost::asio::ip::tcp::socket> socket;
+	Packet packet;
 };
 class Client {
 public :
 	Client(boost::asio::io_service& ios) : ios_(ios) {
 	}
-	template<typename completionHandler>
-	void connect(const std::string& raw_ip_address, const unsigned short& port_num, completionHandler& handler) {
+	void connect(const std::string& raw_ip_address, const unsigned short& port_num) {
 		// required element to connection request.
 		// endpoint - server
 		// socket - client, myself, must be alive for connect operation complete.
@@ -35,7 +70,8 @@ public :
 		session_->socket = sock;
 
 		std::cout << "try to connect " << ep.address().to_string() << ":" << ep.port() << std::endl;
-		sock->async_connect(ep, boost::bind<void>(handler, boost::placeholders::_1, session_));
+		sock->async_connect(ep, 
+			boost::bind(&Client::connect_handler, this, boost::placeholders::_1, session_));
 	}
 	void close() {
 
@@ -44,10 +80,6 @@ public :
 		send_header(message);
 		send_data(message);
 	}
-	void receive() {
-		receive_header();
-		receive_data();
-	}
 protected :
 	void send_header(const std::string& message) {
 
@@ -55,11 +87,44 @@ protected :
 	void send_data(const std::string& message) {
 
 	}
-	void receive_header() {
-
+	void receive_header(std::shared_ptr<Session>& session) {
+		// asynchronously receive packet header.
+		session->socket->async_receive(boost::asio::buffer((char*)&session->packet.header, sizeof(Header)),
+			boost::bind(&Client::receive_header_handler, this, boost::placeholders::_1, boost::placeholders::_2, session));
 	}
-	void receive_data() {
+	void receive_data(std::shared_ptr<Session>& session) {
+		// prepare packet data buffer.
+		session->packet.data.buffer = std::shared_ptr<unsigned char>(new unsigned char[session->packet.header.data_size], std::default_delete<unsigned char[]>());
 
+		// asynchronously receive packet data.
+		session->socket->async_receive(boost::asio::buffer((char*)session->packet.data.buffer.get(), sizeof(Header)),
+			boost::bind(&Client::receive_header_handler, this, boost::placeholders::_1, boost::placeholders::_2, session));
+	}
+	void connect_handler(const boost::system::error_code& e, std::shared_ptr<Session>& session) {
+		if (e.value() != 0) {
+			std::cerr << "CONNECT ERROR[" << e.value() << "] " << e.message() << std::endl;
+			return;
+		}
+		boost::asio::ip::tcp::endpoint& remote = session->socket->remote_endpoint();
+		boost::asio::ip::tcp::endpoint& local = session->socket->local_endpoint();
+		std::cout << "successfully connected." << std::endl;
+		std::cout << local.address().to_string() << ":" << local.port() << "----"
+			<< remote.address().to_string() << ":" << remote.port() << std::endl;
+
+		// start receiving packet.
+		receive_header(session);
+	}
+	void receive_header_handler(const boost::system::error_code& e, std::size_t read_size, std::shared_ptr<Session>& session) {
+		if (e.value() != 0) {
+			std::cerr << "RECEIVE HEADER ERROR[" << e.value() << "] " << e.message() << std::endl;
+			return;
+		}
+	}
+	void receive_data_handler(const boost::system::error_code& e, std::size_t read_size, std::shared_ptr<Session>& session) {
+		if (e.value() != 0) {
+			std::cerr << "RECEIVE DATA ERROR[" << e.value() << "] " << e.message() << std::endl;
+			return;
+		}
 	}
 protected :
 	boost::asio::io_service& ios_;
@@ -69,6 +134,7 @@ int main(void) {
 
 	boost::asio::io_service ios;
 
+	std::thread t(boost::bind(&boost::asio::io_service::run, &ios));
 
 	std::string server_ip_address("127.0.0.1");
 	unsigned short server_port(33333);
@@ -76,13 +142,13 @@ int main(void) {
 	try {
 		Client client(ios);
 
-		client.connect(server_ip_address, server_port, [&](boost::system::error_code e, std::shared_ptr<Session>& s) { 
-			if (e.value() != 0) {
-				throw e;
-			}
-			std::cout << "successfully connected to " << s->socket->remote_endpoint().address().to_string() << ":" << s->socket->remote_endpoint().port() << std::endl;
+		client.connect(server_ip_address, server_port);
 
-			client.receive(); 
+		{
+
+			//std::cout << "successfully connected to " << s->socket->remote_endpoint().address().to_string() << ":" << s->socket->remote_endpoint().port() << std::endl;
+			//std::cout << "my socket information : " << s->socket->local_endpoint().address().to_string() << ":" << s->socket->local_endpoint().port() << std::endl;
+
 
 			std::string message;
 			do {
@@ -92,10 +158,10 @@ int main(void) {
 				client.send(message);
 
 			} while (message != "quit");
-		});
+		}
 
 		client.close();
-		ios.run();
+		t.join();
 	}
 	catch (boost::system::error_code& e) {
 		std::cerr << "ERROR[" << e.value() << "] " << e.message() << std::endl;
--- network/server/server.cpp
+++ network/server/server.cpp
@@ -1,41 +1,135 @@
 #define _WIN32_WINNT 0x0601
 #include <list>
+#include <thread>
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 
+#pragma pack(1)
+
+struct Header {
+	unsigned long marker;
+	unsigned long data_type;
+	unsigned long data_size;
+};
+struct Data {
+	unsigned long marker;
+	unsigned long data_type;
+	std::shared_ptr<unsigned char> buffer;
+};
+struct Packet {
+	Header header;
+	Data data;
+};
+struct Session {
+	std::shared_ptr<boost::asio::ip::tcp::socket> socket;
+	Packet packet;
+};
 class Server {
 public :
 	Server(boost::asio::io_service& ios) : ios_(ios), acceptor_(ios), BACKLOG(5) {
-		ios_.run();
 	}
 	void open(const unsigned short& port_num) {
 		// requirement of server open
 		// endpoint - host, all-ip, specific-port
 		// socket 
 		// acceptor
-		boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address_v4(), port_num);
-		std::shared_ptr<boost::asio::ip::tcp::socket> socket(std::make_shared<boost::asio::ip::tcp::socket>(ios_));
-
+		boost::asio::ip::tcp::endpoint ep(boost::asio::ip::tcp::v4(), port_num);
+		acceptor_.open(ep.protocol());
 		acceptor_.bind(ep);
 
 		acceptor_.listen(BACKLOG);
 
-		acceptor_.async_accept(*socket, boost::bind(&Server::accept_handler, this, boost::placeholders::_1));
+		std::cout << acceptor_.local_endpoint().address().to_string() << ":" << acceptor_.local_endpoint().port() << " listening..." << std::endl;
 
+		accept();
+	}
+	void run() {
+		ios_.run();
 	}
 	void close() {
 
 	}
-	void accept_handler(boost::system::error_code e) {
+private :
+	void accept() {
+		std::shared_ptr<Session> session(std::make_shared<Session>());
+		std::shared_ptr<boost::asio::ip::tcp::socket> socket(std::make_shared<boost::asio::ip::tcp::socket>(ios_));
+		session->socket = socket;
 
+		acceptor_.async_accept(*socket,
+			boost::bind(&Server::accept_handler, this, boost::placeholders::_1, session));
+	}
+	void receive(std::shared_ptr<Session>& session) {
+		boost::asio::ip::tcp::socket& socket = *(session->socket);
+
+		// asynchronously receive header.
+		socket.async_receive(boost::asio::buffer((char*)&session->packet.header, sizeof(Packet)),
+			boost::bind(&Server::receive_header_handler, this, boost::placeholders::_1, boost::placeholders::_2, session));
+	}
+	void send(std::shared_ptr<Session>& session) {
+
+	}
+	void accept_handler(const boost::system::error_code& e, std::shared_ptr<Session>& session) {
+		if (e.value() != 0) {
+			std::cerr << "ACCEPT ERROR[" << e.value() << "] " << e.message() << std::endl;
+			return;
+		}
+
+		const boost::asio::ip::tcp::endpoint& remote = session->socket->remote_endpoint();
+		std::cout << remote.address().to_string() << ":" << remote.port() << " connected." << std::endl;
+
+		sessionlist.push_back(session);
+
+		receive(session);
+
+		accept();
+	}
+	void receive_header_handler(const boost::system::error_code& e, std::size_t read_size, std::shared_ptr<Session>& session) {
+		if (e.value() != 0) {
+			std::cerr << "RECEIVE HEADER ERROR[" << e.value() << "] " << e.message() << std::endl;
+			return;
+		}
+		// check marker of header.
+		unsigned long validation = 0x01020304;
+		Header& header = session->packet.header;
+		if (::memcmp(&header.marker, &validation, sizeof(unsigned long)) != 0) {
+			std::cerr << "INVALID MARKER" << std::endl;
+			return;
+		}
+		// check data type. (not yet)
+		// prepare buffer.
+		session->packet.data.buffer = std::shared_ptr<unsigned char>(new unsigned char[session->packet.header.data_size], std::default_delete<unsigned char[]>());
+
+		// asynchronously receive data
+		session->socket->async_receive(boost::asio::buffer((char*)session->packet.data.buffer.get(), session->packet.header.data_size + sizeof(unsigned long)*2),
+			boost::bind(&Server::receive_data_handler, this, boost::placeholders::_1, boost::placeholders::_2, session));
+	}
+	void receive_data_handler(const boost::system::error_code& e, std::size_t read_size, std::shared_ptr<Session>& session) {
+		if (e.value() != 0) {
+			std::cerr << "RECEIVE DATA ERROR[" << e.value() << "] " << e.message() << std::endl;
+			return;
+		}
+		// check marker of data.
+		// check data-type
+		unsigned long validation = 0x04030201;
+		Data& data = session->packet.data;
+		if (::memcmp(&data.marker, &validation, sizeof(unsigned long)) != 0) {
+			std::cerr << "INVALID MARKER" << std::endl;
+			return;
+		}
+		// check data type. (not yet)
+		// process read-buffer.
+		std::string buffer(reinterpret_cast<char*>(data.buffer.get()), reinterpret_cast<char*>(data.buffer.get()) + session->packet.header.data_size);
+		std::cout << "R> " << buffer << std::endl;
+
+		receive(session);
 	}
 
 private :
 	boost::asio::io_service& ios_;
 	boost::asio::ip::tcp::acceptor acceptor_;
 	int BACKLOG;
-	std::list<std::shared_ptr<boost::asio::ip::tcp::socket>> socketlist;
+	std::list<std::shared_ptr<Session>> sessionlist;
 };
 int main(void) {
 	boost::asio::io_service ios;
@@ -45,6 +139,8 @@ int main(void) {
 	unsigned short port_num(33333);
 
 	server.open(port_num);
+
+	server.run();
 
 	return 0;
 }
