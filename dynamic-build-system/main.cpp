ce6ff9e4c25e5789d677ffc3b2cc23113c915d50
Jong Kyu, Lim <jklim1253@gmail.com>
2017-04-18 21:33:40 +0900

    util namespace. error_code enum. make_log. etc...

--- dynamic-build-system/main.cpp
+++ dynamic-build-system/main.cpp
@@ -1,6 +1,42 @@
+#include <algorithm>
 #include <iostream>
 #include <functional>
+#include <string>
 #include <list>
+#include <deque>
+
+enum error_code
+{
+    no_error = 0,
+
+    CRITICAL_ERROR = 0x40000000,
+};
+
+namespace util
+{
+    typedef std::list<std::wstring> token_type;
+
+    token_type tokenize(const std::wstring& src, const std::wstring& delimiter = L" \t\n")
+    {
+        token_type tokens;
+        bool need_to_ticket = true;
+        std::for_each(src.cbegin(), src.cend(), [&](std::wstring::value_type c) {
+                if (delimiter.cend() == std::find(delimiter.cbegin(), delimiter.cend(), c))
+                {
+                    if (need_to_ticket)
+                    {
+                        tokens.push_back(std::wstring());
+                        need_to_ticket = false;
+                    }
+                    tokens.back().append(c, 1);
+                }
+                else
+                {
+                    need_to_ticket = true;
+                }
+            });
+    }
+}
 
 struct ISystem
 {
@@ -14,31 +50,40 @@ private :
     std::wstring m_name;
 };
 
+struct critical_section
+{
+};
+
 template<typename LogType>
 struct ILogReceiver
 {
-    virtual void OnWrite(int type, LogType& log)
+    virtual void OnWrite(LogType log)
     {
         // TODO : implementation of log record.
+        depot.push_back(log);
     }
+
+protected :
+    std::deque<LogType> depot;
+    mutable critical_section cs;
 };
 
 template<typename LogType, typename ...Elements>
 LogType make_log(Elements&&... elements)
 {
-
+    return LogType(elements...);
 }
 
 template<typename LogType>
 struct ILogSender
 {
-    virtual void write(int type, std::wstring& log)
+    virtual void write(int type, const std::wstring& log)
     {
-        Logger.OnWrite(type, make_log(type, 0.0, log, this));
+        Logger.OnWrite(make_log(type, 0.0, log, this));
     }
 
 private :
-    static ILogReceiver Logger;
+    static ILogReceiver<LogType> Logger;
 };
 
 struct LogData
@@ -63,6 +108,7 @@ public :
         return 0;
     }
 };
+
 class MotorSystem : public ISystem
 {
 public :
@@ -77,6 +123,7 @@ public :
         return 0;
     }
 };
+
 class VisionSystem : public ISystem
 {
 public :
@@ -91,6 +138,22 @@ public :
         return 0;
     }
 };
+
+class TeachingSystem : public ISystem
+{
+public :
+    TeachingSystem() { name(L"Teaching-System"); }
+    // Inherited via ISystem
+    virtual int Initialize() override
+    {
+        return 0;
+    }
+    virtual int Release() override
+    {
+        return 0;
+    }
+};
+
 class Application : public ISystem, public ILogSender<LogData>
 {
 public :
@@ -122,7 +185,7 @@ public :
         write(0, L"start to initialize all systems");
         for (auto sys : systems)
         {
-            if (sys->Initialize())
+            if (sys->Initialize() == no_error)
             {
                 std::wcout << sys->name().c_str() << L" initialized successfully." << std::endl;
             }
@@ -151,6 +214,24 @@ public :
         std::wcout << L"releasing all systems finished" << std::endl;
         return 0;
     }
+
+    int parse_command_line(const std::wstring& command)
+    {
+        util::token_type tokens = util::tokenize(command);
+        return 0;
+    }
+
+    void error_process(int error_code)
+    {
+        if ((error_code & CRITICAL_ERROR) == CRITICAL_ERROR)
+        {
+            write(0, L"[CERR] xxx");
+        }
+        else
+        {
+            write(0, L"[ERR] xxx");
+        }
+    }
 };
 
 int main()
@@ -159,6 +240,15 @@ int main()
 
     app.Initialize();
 
+    std::wstring command;
+    while (std::getline(std::wcin, command) && command != L"quit")
+    {
+        int ret = app.parse_command_line(command);
+        if (ret != no_error)
+        {
+            app.error_process(ret);
+        }
+    }
 
     app.Release();
 
