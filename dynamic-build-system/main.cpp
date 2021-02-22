2847bd62d665a829106ad428ad3bd37353b06126
Jong Kyu, Lim <jklim1253@gmail.com>
2021-02-23 02:05:04 +0900

    cmake applied

--- dynamic-build-system/main.cpp
+++ dynamic-build-system/main.cpp
@@ -3,51 +3,85 @@
 #include <functional>
 #include <string>
 #include <list>
+#include <vector>
 #include <deque>
+#include <mutex>
 
+namespace ec
+{
 enum error_code
 {
-    no_error = 0,
+  no_error = 0,
+  not_implemented_yet,
 
-    CRITICAL_ERROR = 0x40000000,
+  // system
+  failed_to_initialize,
+  failed_to_release,
+
+  critical_error = 0x40000000,
 };
+} // namespace ec
+
+namespace ac
+{
+enum application_code
+{
+  idle = 0,
+  need_to_stop,
+  running,
+  no_response,
+};
+} // namespace ac
 
 namespace util
 {
-    typedef std::list<std::wstring> token_type;
+  using token_type = std::vector<std::string>;
 
-    token_type tokenize(const std::wstring& src, const std::wstring& delimiter = L" \t\n")
+  token_type tokenize(std::string const& src, std::string const& delimiter = " \t\n")
+  {
+    token_type tokens;
+    bool need_to_ticket = true;
+
+    for (auto const& c : src)
     {
-        token_type tokens;
-        bool need_to_ticket = true;
-        std::for_each(src.cbegin(), src.cend(), [&](std::wstring::value_type c) {
-                if (delimiter.cend() == std::find(delimiter.cbegin(), delimiter.cend(), c))
-                {
-                    if (need_to_ticket)
-                    {
-                        tokens.push_back(std::wstring());
-                        need_to_ticket = false;
-                    }
-                    tokens.back().append(c, 1);
-                }
-                else
-                {
-                    need_to_ticket = true;
-                }
-            });
+      if (delimiter.cend() == std::find(delimiter.cbegin(), delimiter.cend(), c))
+      {
+        if (need_to_ticket)
+        {
+          tokens.push_back(std::string());
+          need_to_ticket = false;
+        }
+        tokens.back().append(1, c);
+      }
+      else
+      {
+        need_to_ticket = true;
+      }
     }
+
+    return tokens;
+  }
 }
 
 struct ISystem
 {
-    virtual int Initialize() = 0;
-    virtual int Release() = 0;
+  virtual int Initialize() = 0;
+  virtual int Release() = 0;
 
-    std::wstring name() const { return m_name; }
-    void name(const std::wstring& in_name) { m_name = in_name; }
+  ISystem(std::string const& _name)
+    : m_name(_name)
+  {
+  }
+  virtual ~ISystem()
+  {
+  }
+  std::string name() const
+  {
+    return m_name;
+  }
 
 private :
-    std::wstring m_name;
+  std::string m_name;
 };
 
 struct critical_section
@@ -57,200 +91,252 @@ struct critical_section
 template<typename LogType>
 struct ILogReceiver
 {
-    virtual void OnWrite(LogType log)
-    {
-        // TODO : implementation of log record.
-        depot.push_back(log);
-    }
+  virtual void OnWrite(LogType log)
+  {
+    // TODO : implementation of log record.
+    depot.push_back(log);
+  }
 
 protected :
-    std::deque<LogType> depot;
-    mutable critical_section cs;
+  std::deque<LogType> depot;
+  mutable critical_section cs;
 };
 
 template<typename LogType, typename ...Elements>
 LogType make_log(Elements&&... elements)
 {
-    return LogType(elements...);
+  return LogType{elements...};
 }
 
 template<typename LogType>
 struct ILogSender
 {
-    virtual void write(int type, const std::wstring& log)
-    {
-        Logger.OnWrite(make_log(type, 0.0, log, this));
-    }
+  virtual void write(int type, const std::string& log)
+  {
+    Logger.OnWrite(make_log<LogType>(type, 0.0, log, this));
+  }
 
 private :
-    static ILogReceiver<LogType> Logger;
+  static ILogReceiver<LogType> Logger;
 };
+template<typename LogType>
+ILogReceiver<LogType> ILogSender<LogType>::Logger;
 
 struct LogData
 {
-    int type;
-    double time;
-    std::wstring msg;
-    ILogSender<LogData>* sender;
+  int type;
+  double time;
+  std::string msg;
+  ILogSender<LogData>* sender;
 };
 
-class IOSystem : public ISystem
+class IOSystem
+  : public ISystem
 {
 public :
-    IOSystem() { name(L"DIO-System"); }
-    // Inherited via ISystem
-    virtual int Initialize() override
-    {
-        return 0;
-    }
-    virtual int Release() override
-    {
-        return 0;
-    }
+  IOSystem()
+    : ISystem("IOSystem")
+  {
+  }
+  // Inherited via ISystem
+  virtual int Initialize() override
+  {
+    return ec::not_implemented_yet;
+  }
+  virtual int Release() override
+  {
+    return ec::not_implemented_yet;
+  }
 };
 
-class MotorSystem : public ISystem
+class MotorSystem
+  : public ISystem
 {
 public :
-    MotorSystem() { name(L"Motor-System"); }
-    // Inherited via ISystem
-    virtual int Initialize() override
-    {
-        return 0;
-    }
-    virtual int Release() override
-    {
-        return 0;
-    }
+  MotorSystem()
+    : ISystem("MotorSystem")
+  {
+  }
+  // Inherited via ISystem
+  virtual int Initialize() override
+  {
+    return ec::not_implemented_yet;
+  }
+  virtual int Release() override
+  {
+    return ec::not_implemented_yet;
+  }
 };
 
-class VisionSystem : public ISystem
+class VisionSystem
+  : public ISystem
 {
 public :
-    VisionSystem() { name(L"Vision-System"); }
-    // Inherited via ISystem
-    virtual int Initialize() override
-    {
-        return 0;
-    }
-    virtual int Release() override
-    {
-        return 0;
-    }
+  VisionSystem()
+    : ISystem("Vision-System")
+  {
+  }
+  // Inherited via ISystem
+  virtual int Initialize() override
+  {
+    return ec::not_implemented_yet;
+  }
+  virtual int Release() override
+  {
+    return ec::not_implemented_yet;
+  }
 };
 
-class TeachingSystem : public ISystem
+class TeachingSystem
+  : public ISystem
 {
 public :
-    TeachingSystem() { name(L"Teaching-System"); }
-    // Inherited via ISystem
-    virtual int Initialize() override
-    {
-        return 0;
-    }
-    virtual int Release() override
-    {
-        return 0;
-    }
+  TeachingSystem()
+    : ISystem("Teaching-System")
+  {
+  }
+  // Inherited via ISystem
+  virtual int Initialize() override
+  {
+    return ec::not_implemented_yet;
+  }
+  virtual int Release() override
+  {
+    return ec::not_implemented_yet;
+  }
 };
 
-class Application : public ISystem, public ILogSender<LogData>
+class Application
+  : public ISystem
+  , public ILogSender<LogData>
 {
 public :
-    template<typename First, typename ...Others>
-    Application(First&& sys, Others&&... others)
-    {
-        _addsystem(sys, others...);
-    }
+  template<typename First, typename ...Others>
+  Application(First&& sys, Others&&... others)
+    : ISystem("Application")
+    , m_state(ac::idle)
+  {
+    _addsystem(sys, others...);
+  }
 
 private :
-    template<typename First, typename ...Others>
-    void _addsystem(First&& sys, Others&&... others)
-    {
-        systems.push_back(sys);
-        _addsystem(others...);
-    }
-    template<typename Last>
-    void _addsystem(Last&& sys)
-    {
-        systems.push_back(sys);
-    }
+  template<typename First, typename ...Others>
+  void _addsystem(First&& first, Others&&... others)
+  {
+    systems.push_back(first);
+    _addsystem(others...);
+  }
+  template<typename Last>
+  void _addsystem(Last&& last)
+  {
+    systems.push_back(last);
+  }
 private :
-    std::list<ISystem*> systems;
+  std::list<ISystem*> systems;
+  int m_state;
 
 public :
-    // Inherited via ISystem
-    virtual int Initialize() override
+  // Inherited via ISystem
+  virtual int Initialize() override
+  {
+    write(0, "start to initialize all systems");
+    int fault_count = 0;
+    for (auto sys : systems)
     {
-        write(0, L"start to initialize all systems");
-        for (auto sys : systems)
-        {
-            if (sys->Initialize() == no_error)
-            {
-                std::wcout << sys->name().c_str() << L" initialized successfully." << std::endl;
-            }
-            else
-            {
-                std::wcout << sys->name().c_str() << L" initialization failed." << std::endl;
-            }
-        }
-        std::wcout << L"initialization all systems finished" << std::endl;
-        return 0;
+      if (sys->Initialize() == ec::no_error)
+      {
+        std::cout << sys->name().c_str() << " initialized successfully." << std::endl;
+      }
+      else
+      {
+        std::cout << sys->name().c_str() << " initialization failed." << std::endl;
+        ++fault_count;
+      }
     }
-    virtual int Release() override
-    {
-        std::wcout << L"start to release all systems" << std::endl;
-        for (auto sys : systems)
-        {
-            if (sys->Release())
-            {
-                std::wcout << sys->name().c_str() << L" released successfully." << std::endl;
-            }
-            else
-            {
-                std::wcout << sys->name().c_str() << L" releasing failed." << std::endl;
-            }
-        }
-        std::wcout << L"releasing all systems finished" << std::endl;
-        return 0;
-    }
+    std::cout << "initialization all systems finished" << std::endl;
 
-    int parse_command_line(const std::wstring& command)
+    if (fault_count != 0)
     {
-        util::token_type tokens = util::tokenize(command);
-        return 0;
+      return ec::failed_to_initialize;
     }
+    return ec::no_error;
+  }
+  virtual int Release() override
+  {
+    std::cout << "start to release all systems" << std::endl;
+    int fault_count = 0;
+    for (auto sys : systems)
+    {
+      if (sys->Release() == ec::no_error)
+      {
+        std::cout << sys->name().c_str() << " released successfully." << std::endl;
+      }
+      else
+      {
+        std::cout << sys->name().c_str() << " releasing failed." << std::endl;
+        ++fault_count;
+      }
+    }
+    std::cout << "releasing all systems finished" << std::endl;
 
-    void error_process(int error_code)
+    if (fault_count != 0)
     {
-        if ((error_code & CRITICAL_ERROR) == CRITICAL_ERROR)
-        {
-            write(0, L"[CERR] xxx");
-        }
-        else
-        {
-            write(0, L"[ERR] xxx");
-        }
+      return ec::failed_to_release;
     }
+    return ec::no_error;
+  }
+
+  int parse_command_line(const std::string& command)
+  {
+    util::token_type tokens = util::tokenize(command);
+    if (tokens.size() == 1 && tokens[0] == "quit")
+    {
+      m_state = ac::need_to_stop;
+    }
+    return 0;
+  }
+
+  void error_process(int error_code)
+  {
+    if ((error_code & ec::critical_error) == ec::critical_error)
+    {
+      write(0, "[CERR] xxx");
+    }
+    else
+    {
+      write(0, "[ERR] xxx");
+    }
+  }
+  int state() const
+  {
+    return m_state;
+  }
 };
 
 int main()
 {
-    Application app(new IOSystem, new MotorSystem, new VisionSystem);
+  Application app(new IOSystem, new MotorSystem, new VisionSystem);
 
-    app.Initialize();
+  app.Initialize();
 
-    std::wstring command;
-    while (std::getline(std::wcin, command) && command != L"quit")
+  std::string command;
+  while (ac::need_to_stop != app.state())
+  {
+    std::cout << "command:> ";
+    std::getline(std::cin, command);
+
+    int ret = app.parse_command_line(command);
+    if (ret != ec::no_error)
     {
-        int ret = app.parse_command_line(command);
-        if (ret != no_error)
-        {
-            app.error_process(ret);
-        }
+      app.error_process(ret);
     }
+  }
 
-    app.Release();
+  app.Release();
 
-    return 0;
+#ifdef _DEBUG
+  std::cout << "presss enter to exit...";
+  std::cin.get();
+#endif
+  return 0;
 }
